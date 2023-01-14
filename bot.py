60215381a6a6c18bea73d8472fbaff09853028e9
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2023-01-14 09:03:01 -0500

    save

--- bot.py
+++ bot.py
@@ -13,8 +13,6 @@ class Bot:
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
 
-        print(game_message.playAreas)
-
         other_team_ids = [
             team for team in game_message.teams if team != game_message.teamId]
         actions = list()
@@ -35,8 +33,24 @@ class Bot:
     #SPEAR_SHOOTER = "SPEAR_SHOOTER"
 
     def trouver_rayon_attaque(self, tiles_path: list, position: Position, type_tower: TowerType):
-        liste_shoot = []
+        liste_shoot_x = []
+        liste_shoot_y = []
+        liste_position_attaque = []
+        liste_loops = [-2, -1, 0, 1, 2]
         if type_tower == "SPEAR_SHOOTER":
+            for i in liste_loops:
+                if (position.x + i >= 0):
+                    liste_shoot_x.append(position.x + i)
+            for i in liste_loops:
+                if (position.x + i >= 0):
+                    liste_shoot_y.append(position.y + i)
+            for position_x in liste_shoot_x:
+                for position_y in liste_shoot_y:
+                    liste_position_attaque.append(
+                        Position(position_x, position_y))
+
+            liste_shoot_x.append(position.x - 2)
+            coin_sup_droit_y = position.y + 2
 
             pass
         elif type_tower == "BOMB_SHOOTER":
@@ -50,13 +64,23 @@ class Bot:
         possible_positions = []
         for x in range(GameMessage.map.width):
             for y in range(GameMessage.map.height):
-                pass
-        return
+                next_position = Position()
+                next_position.x, next_position.y = x, y
+                if not game_message.playAreas[game_message.teamId].get_tile_at(next_position).hasObstacle:
+                    possible_positions.append(next_position)
+        return possible_positions
+
+    def _get_best_tower(self, possiblePosition: List[Position]):
+
+        # Contient les 3 tours avec tous les rayson d'attaque de ces tours
+        rayonAction = {}
+        bestPositionForEachTower = {}
+
+        for position in possiblePosition:
+            for towerType in TowerType:
+                rayonAction[towerType] = {
+                    'position': position, 'rayonAction': self.trouver_rayon_attaque(position, towerType)}
 
 
-def _get_best_tower(self,):
-
-    rayonAction = {}
-
-    for towerType in TowerType:
-        rayonAction[towerType] = self.trouver_rayon_attaque(towerType)
+def evaluate_function(rayonsDAttaque: list[Position]):
+    best
