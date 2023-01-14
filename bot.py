0c8d9d28ce74ec64a68ac963920fe47b7b7032df
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2023-01-14 08:26:58 -0500

    yes

--- bot.py
+++ bot.py
@@ -11,13 +11,21 @@ class Bot:
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
 
-        other_team_ids = [team for team in game_message.teams if team != game_message.teamId]
+        other_team_ids = [
+            team for team in game_message.teams if team != game_message.teamId]
         actions = list()
 
-        actions.append(SellAction(Position(0, 0)))
-        actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(0, 0)))
+        actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(
+            game_message.map.paths.tiles[6].x+1, game_message.map.paths.tiles[6].y-1)))
+        actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(
+            game_message.map.paths.tiles[15].x+1, game_message.map.paths.tiles[15].y-1)))
+        actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(17, 20)))
+
+        #actions.append(SellAction(Position(0, 0)))
+        #actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(0, 0)))
 
         if other_team_ids:
-            actions.append(SendReinforcementsAction(EnemyType.LVL1, other_team_ids[0]))
+            actions.append(SendReinforcementsAction(
+                EnemyType.LVL1, other_team_ids[0]))
 
         return actions
