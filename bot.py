b4d392c0ad4e9399e522b9f50600da05b57bf344
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2023-01-14 09:24:50 -0500

    v1

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 from game_message import *
-from game_message import GameMessage, TowerType, Position, EnemyType, Tower, PlayArea
+from game_message import GameMessage, TowerType, BuildAction, Position, SendReinforcementsAction, EnemyType, Tower, PlayArea
 from actions import *
 import random
+from typing import List
 
 
 class Bot:
     def __init__(self):
+
         print("Initializing your super mega duper bot")
 
     def get_next_move(self, game_message: GameMessage):
@@ -18,48 +20,48 @@ class Bot:
         actions = list()
 
         actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(
-            random.randint(1, game_message.map.width), random.randint(1, game_message.map.height))))
+            random.randint(1, map.width), random.randint(1, map.height))))
 
-        #actions.append(SellAction(Position(0, 0)))
-        #actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(0, 0)))
+        # actions.append(SellAction(Position(0, 0)))
+        # actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(0, 0)))
 
         if other_team_ids:
             actions.append(SendReinforcementsAction(
                 EnemyType.LVL1, other_team_ids[0]))
 
         return actions
-    #SPIKE_SHOOTER = "SPIKE_SHOOTER"
-    #BOMB_SHOOTER = "BOMB_SHOOTER"
-    #SPEAR_SHOOTER = "SPEAR_SHOOTER"
+    # SPIKE_SHOOTER = "SPIKE_SHOOTER"
+    # BOMB_SHOOTER = "BOMB_SHOOTER"
+    # SPEAR_SHOOTER = "SPEAR_SHOOTER"
 
     def trouver_rayon_attaque(self, tiles_path: list, position: Position, type_tower: TowerType):
         liste_shoot_x = []
         liste_shoot_y = []
         rayon_attaque = []
+        tiles_rayon = []
         liste_loops = [-2, -1, 0, 1, 2]
-        if type_tower == "SPEAR_SHOOTER":
-            for i in liste_loops:
-                if (position.x + i >= 0):
-                    liste_shoot_x.append(position.x + i)
-            for i in liste_loops:
-                if (position.x + i >= 0):
-                    liste_shoot_y.append(position.y + i)
-            for position_x in liste_shoot_x:
-                for position_y in liste_shoot_y:
-                    liste_position_attaque.append(
-                        Position(position_x, position_y))
+        if type_tower == "SPEAR_SHOOTER" or type_tower == "BOMB_SHOOTER":
+            liste_loops = [-2, -1, 0, 1, 2]
 
-            for tile in rayon_attaque:
-
-            liste_shoot_x.append(position.x - 2)
-            coin_sup_droit_y = position.y + 2
-
-            pass
         elif type_tower == "BOMB_SHOOTER":
-            pass
-        else:
-            pass
-        return
+            liste_loop = [-1, 0, 1]
+
+        for i in liste_loops:
+            if (position.x + i >= 0):
+                liste_shoot_x.append(position.x + i)
+        for i in liste_loops:
+            if (position.x + i >= 0):
+                liste_shoot_y.append(position.y + i)
+        for position_x in liste_shoot_x:
+            for position_y in liste_shoot_y:
+                rayon_attaque.append(
+                    Position(position_x, position_y))
+
+        for tile in rayon_attaque:
+            if tile in tiles_path:
+                tiles_rayon.append(tile)
+
+        return tiles_rayon
 
     def _get_possible_positions(self, game_message: GameMessage):
         # creates a list containing all useable positions to place a tower
@@ -68,22 +70,48 @@ class Bot:
             for y in range(GameMessage.map.height):
                 next_position = Position()
                 next_position.x, next_position.y = x, y
-                if not game_message.playAreas[game_message.teamId].get_tile_at(next_position).hasObstacle:
+                if not game_message.playAreas[game_message.teamId].get_tile_at(next_position).hasObstacle and not self._in_path(game_message, next_position):
                     possible_positions.append(next_position)
         return possible_positions
 
-    def _get_best_tower(self, possiblePosition: List[Position]):
+    def _in_path(self, game_message: GameMessage, position: Position):
+        for path in game_message.map.paths:
+            for pos in path.tiles:
+                if pos.x == position.x and pos.y == position.y:
+                    return True
+        return False
+
+    def _get_best_tower(self, possiblePosition: List[Position], game_message: GameMessage)):
 
         # Contient les 3 tours avec tous les rayson d'attaque de ces tours
-        rayonAction = {}
-        bestPositionForEachTower = {}
+        rayonAction={}
+        bestPositionForEachTower={}
+
+        allPaths=[]
+        for path in game_message.map.paths:
+            for tile in path.tiles:
+                if tile not in allPaths:
+                    allPaths.append(path.tiles)
+
 
         for position in possiblePosition:
             for towerType in TowerType:
-                for
+                rayonAction[towerType]=[]
+                rayonAction[towerType].append(
+                    {'position': position, 'rayonAction': self.trouver_rayon_attaque(allPaths, position, towerType)})
+        for towerType in TowerType:
+            bestPositionForEachTower[towerType]=evaluate_function(
+                rayonAction[towerType].rayonAction)
 
-                # 'position': position, 'rayonAction': self.trouver_rayon_attaque(position, towerType)}
+        return bestPositionForEachTower
 
 
 def evaluate_function(rayonsDAttaque: list[Position]):
-    best
+    maxTouchedTile = 0
+    bestPosition = Position()
+    for rayonAction in rayonsDAttaque:
+        if len(rayonAction) > maxTouchedTile:
+            maxTouchedTile = len(rayonAction)
+            bestPosition = rayonAction.position
+
+    return {'position': bestPosition, 'tileTouched': maxTouchedTile}
